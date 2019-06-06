b3f92963d2fcc5d8dcb0c4a2d7b217df5ca22eb8
鈴木響 <h.suzuki@fdc-inc.co.jp>
2019-06-06 15:43:38 +0900

    change 1ユーザに対する2つ以上の権限付与に対応
    User/ Create, Delete, Details, Edit, Index

--- csharp/LunchOrderingSystem/LunchOrderingSystem/Controllers/UserController.cs
+++ csharp/LunchOrderingSystem/LunchOrderingSystem/Controllers/UserController.cs
@@ -18,7 +18,7 @@ namespace LunchOrderingSystem.Controllers
         // GET: User
         public ActionResult Index()
         {
-            return View(db.m_user.ToList());
+            return View(db.m_user.Include(p => p.m_user_role).ToList());
         }
 
         // GET: User/Details/5
@@ -47,14 +47,34 @@ namespace LunchOrderingSystem.Controllers
         // 詳細については、https://go.microsoft.com/fwlink/?LinkId=317598 を参照してください。
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "id,login_id,login_password,role,name")] m_user m_user)
+        public ActionResult Create([Bind(Include = "id,login_id,login_password,name")] m_user m_user, string[] roles)
         {
+            if (roles == null) roles = new string[0];
             if (ModelState.IsValid)
             {
-                m_user.login_password = BCrypt.Net.BCrypt.HashPassword(m_user.login_password);
-                db.m_user.Add(m_user);
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                var user = db.m_user.Where(u => u.login_id == m_user.login_id).FirstOrDefault();
+                if(user == null)
+                {
+                    m_user.role = ""; //暫定的処理
+                    m_user.login_password = BCrypt.Net.BCrypt.HashPassword(m_user.login_password);
+                    db.m_user.Add(m_user);
+                    db.SaveChanges();
+
+                    user = db.m_user.Where(u => u.login_id == m_user.login_id).FirstOrDefault();
+
+                    foreach(var roleName in roles)
+                    {
+                        var role = new m_user_role
+                        {
+                            user_id = user.id,
+                            role = roleName
+                        };
+                        db.m_user_role.Add(role);
+                        db.SaveChanges();
+                    }
+
+                    return RedirectToAction("Index");
+                }
             }
 
             return View(m_user);
@@ -80,7 +100,7 @@ namespace LunchOrderingSystem.Controllers
         // 詳細については、https://go.microsoft.com/fwlink/?LinkId=317598 を参照してください。
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "id,login_id,name")] m_user m_user)
+        public ActionResult Edit([Bind(Include = "id,login_id,name")] m_user m_user, string[] roles)
         {
             if (ModelState.IsValid)
             {
@@ -90,6 +110,40 @@ namespace LunchOrderingSystem.Controllers
                     u.login_id = m_user.login_id;
                     u.name = m_user.name;
                     db.SaveChanges();
+
+                    var dbRoleList = db.m_user_role.Where(r => r.user_id == m_user.id)
+                        .Select(r => r.role).ToList();
+
+                    if (roles == null) roles = new string[0];
+                    var roleList = roles.ToList();
+                    foreach(var r in roles)
+                    {
+                        if (dbRoleList.Contains(r))
+                        {
+                            roleList.Remove(r);
+                            dbRoleList.Remove(r);
+                        }
+                    }
+
+                    foreach(var r in roleList)
+                    {
+                        var role = new m_user_role
+                        {
+                            user_id = m_user.id,
+                            role = r
+                        };
+                        db.m_user_role.Add(role);
+                        db.SaveChanges();
+                    }
+
+                    foreach (var r in dbRoleList)
+                    {
+                        var role = db.m_user_role
+                            .Where(ur => ur.user_id == m_user.id && ur.role == r)
+                            .FirstOrDefault();
+                        db.m_user_role.Remove(role);
+                        db.SaveChanges();
+                    }
                 }
                 
                 return RedirectToAction("Index");
@@ -156,6 +210,14 @@ namespace LunchOrderingSystem.Controllers
             m_user m_user = db.m_user.Find(id);
             db.m_user.Remove(m_user);
             db.SaveChanges();
+
+            var roles = db.m_user_role.Where(u => u.user_id == id).ToList();
+            foreach(var role in roles)
+            {
+                db.m_user_role.Remove(role);
+                db.SaveChanges();
+            }
+
             return RedirectToAction("Index");
         }
 
