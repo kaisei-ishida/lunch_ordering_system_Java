506890a85f280217163304f7353dc254bba0b4b1
k.ishida <k.ishida@SYS-PS-083>
2019-06-11 14:00:49 +0900

    HomeServletの変更

--- src/HomeServlet.java
+++ src/HomeServlet.java
@@ -7,6 +7,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet("/home")
 public class HomeServlet extends HttpServlet{
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response)
